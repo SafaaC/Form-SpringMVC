41f3b828b061a4ab637292291998c76a3aef3055
SafaaC <safaaacherak@gmail.com>
2023-06-02 15:06:00 +0530

    Replaced RequestParam Annotation with ModelAttribute Annotation

--- src/main/java/com/ContactController.java
+++ src/main/java/com/ContactController.java
@@ -2,32 +2,49 @@ package com;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import model.User;
+
 @Controller
 public class ContactController {
-	
+
 	@RequestMapping("/contact")
 	public String showform() {
 		return "contact";
 	}
-	@RequestMapping(value ="/processform",method=RequestMethod.POST)
-	//Using @RequestParam to get data from View to Controller
-	public String handleform(@RequestParam("userEmail") String email,
-							@RequestParam("userName") String name,
-							@RequestParam("userPassword") String password,
-							Model model){												
-		/*
-		 The argument "userEmail" is the name of input type of email in html form
-		 The incoming value of email from html form will be stored in variable email
-		 */
-		
-		
-		model.addAttribute("name",name);
-		model.addAttribute("email",email);
-		model.addAttribute("password",password);
-		return "success";
-	}
+	
+	//Using @ModelAttribute to get data from View to Controller
+	
+	 @RequestMapping(value ="/processform",method=RequestMethod.POST)
+	  
+	  public String handleform(@ModelAttribute User user, Model model){
+	 //ModelAttribute will automatically bind the HTML form's field with the object and set it in the model
+	
+	  return "success"; 
+	  }
+	
+	 
+	 /*
+	 //Using @RequestParam to get data from View to Controller  
+	 
+	 @RequestMapping(value ="/processform",method=RequestMethod.POST)
+	  
+	  public String handleform(@RequestParam("userEmail") String email,
+	  			@RequestParam("userName") String name,
+	  			@RequestParam("userPassword") String password, Model model){
+	 
+	  //The argument "userEmail" is the name of input type of email in html form The
+	  //incoming value of email from html form will be stored in variable email
+	  
+	
+	  model.addAttribute("name",name); model.addAttribute("email",email);
+	  model.addAttribute("password",password);
+	 
+	  return "success"; 
+	  }
+	 */
 }
--- src/main/webapp/WEB-INF/views/success.jsp
+++ src/main/webapp/WEB-INF/views/success.jsp
@@ -1,15 +1,28 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-    <%@page isELIgnored= "false"%>
-   <!DOCTYPE html>
+	pageEncoding="ISO-8859-1"%>
+<%@page isELIgnored="false"%>
+<!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>Success</title>
+<style>
+.container {
+	width: 45%;
+	border: 1px solid black;
+	margin: 5% auto;
+	padding: 20px;
+	font-size: 15px;
+}
+</style>
 </head>
 <body>
-		<h1>Welcome ${name }</h1>
-		<h2>Your Email id is ${email }</h2>
-		<h1>Secure your password ${password }</h1>
+
+	<h1 style="text-align: center;margin: 5%">Registration Successfull</h1>
+	<div class="container">
+		<h2>Welcome <i>${user.userName}</i></h2>
+		<h2>Your Email id is <i>${user.userEmail }</i></h2>
+		<h2>Secure your password <i>${user.userPassword }</i></h2>
+	</div>
 </body>
 </html>
