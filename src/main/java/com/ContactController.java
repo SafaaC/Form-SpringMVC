7d7cee3e17a8081c57c3a1abb9805fe70bd262ae
SafaaC <safaaacherak@gmail.com>
2023-06-02 12:16:47 +0530

    created html form

---
+++ src/main/java/com/ContactController.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+	
+	@RequestMapping("/contact")
+	public String showform() {
+		return "contact";
+	}
+}
